1769332acffbf8fca7dcb98c403211cb6dbb7a68
João Caetano <118355857+Caetano346@users.noreply.github.com>
2024-03-20 09:51:20 -0400

    Update main.tsx and main.css

--- src/componentes/Main/Main.tsx
+++ src/componentes/Main/Main.tsx
@@ -2,7 +2,14 @@ import './Main.css'
 
 function Main(){
     return(
-        <main>Main</main>
+        <main>
+            <div className='box-estudante'>
+                <img src="https://github.com/Caetano346.png" alt="Foto Estudante" />
+                <h3 className='nome-estudante'>João Victor da Silva Caetano</h3>
+                <p className='descricao-estudante'>Estudante do IFMS nas matérias de Desenvolvimento / Desenvolvimento Web</p>
+                <a href="https://github.com/Caetano346">GitHub</a>
+            </div>
+        </main>
     )
 }
 export default Main

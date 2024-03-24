c40963799d8b62bcb9b8194177ce0caea6e4758d
João Caetano <118355857+Caetano346@users.noreply.github.com>
2024-03-23 21:24:40 -0400

    Update Estudante.tsx

--- src/componentes/Main/Estudante/Estudante.tsx
+++ src/componentes/Main/Estudante/Estudante.tsx
@@ -3,10 +3,10 @@ import './Estudante.css'
 function Estudante() {
     return (
         <div className='box-estudante'>
-                <img src="https://github.com/Caetano346.png" alt="Foto Estudante" />
+                <img src="https://github.com/joaovscaetano.png" alt="Foto Estudante" />
                 <h3 className='nome-estudante'>João Victor da Silva Caetano</h3>
                 <p className='descricao-estudante'>Estudante do IFMS nas matérias de Desenvolvimento - Desenvolvimento Web</p>
-                <a href="https://github.com/Caetano346">GitHub</a>
+                <a href="https://github.com/joaovscaetano">GitHub</a>
             </div>
     )
 }
